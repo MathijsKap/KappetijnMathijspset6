d1b249b8b9c1e096219b312637d7eeeb7156d590
hellvox <33220067+hellvox@users.noreply.github.com>
2017-12-04 22:24:55 +0100

    Basics

--- app/src/main/java/com/example/hellvox/kappetijnmathijspset6/Select.java
+++ app/src/main/java/com/example/hellvox/kappetijnmathijspset6/Select.java
@@ -3,17 +3,11 @@ package com.example.hellvox.kappetijnmathijspset6;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.google.firebase.database.FirebaseDatabase;
-
-public class Home extends AppCompatActivity {
+public class Select extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-
-
+        setContentView(R.layout.activity_select);
     }
 }
---
+++ app/src/main/java/com/example/hellvox/kappetijnmathijspset6/Trivia.java
@@ -0,0 +1,18 @@
+package com.example.hellvox.kappetijnmathijspset6;
+
+/**
+ * Created by HellVox on 4-12-2017.
+ */
+
+public class Trivia {
+
+    public String Question;
+    public String answer;
+
+
+    public Trivia() {}
+
+    public Trivia(String Question, String answer) {
+
+    }
+}
